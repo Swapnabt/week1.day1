e00b0cc83af14e3caff91381de3e378708649c39
Swapnabt <109001378+Swapnabt@users.noreply.github.com>
2022-07-10 02:34:15 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Mobile {
+
+	public void makeCall() {
+		 
+		// TODO Auto-generated method stub
+
+	}
+	  public void sendMsg() {
+		// TODO Auto-generated method stub
+
+	}
+	  public static void main(String[] args) {
+		System.out.println("This is my mobile");
+	}
+	}
